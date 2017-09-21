b7dc6072e417dd90e89f6fde1be71e7a8a029a4e
AgesilausDrako <agesilaus@yahoo.com>
2017-09-20 22:05:49 -0400

    added default search

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -15,6 +15,43 @@ function loginDisplay() {
         $(".inputWrap").css("left", window.innerWidth / 2 - 250);
     }
 }
+var slideshow;
+
+function renderData(data) {
+    $(".imageDisplay").empty();
+    clearInterval(slideshow);
+    var index = 1;
+    var presentationDiv = $("<div>");
+    var returnedName = $("<h1 class='artistName'>").text(data.artObjects[0].principalOrFirstMaker);
+    var returnedTitle = $("<h1 class='artTitle'>").text(data.artObjects[0].title);
+    var returnedImages = $("<img class='artDisplay'>").attr("src", data.artObjects[0].webImage.url);
+    presentationDiv.append(returnedName, returnedTitle, returnedImages);
+    $(".imageDisplay").html(presentationDiv);
+        slideshow = setInterval(function() {
+        presentationDiv = $("<div>");
+        returnedName = $("<h1 class='artistName'>").text(data.artObjects[index].principalOrFirstMaker);
+        returnedTitle = $("<h1 class='artTitle'>").text(data.artObjects[index].title);
+        returnedImages = $("<img class='artDisplay'>").attr("src", data.artObjects[index].webImage.url);
+        presentationDiv.append(returnedName, returnedTitle, returnedImages);
+        $(".imageDisplay").html(presentationDiv);
+        index += 1;
+        if(index === data.artObjects.length) {
+            index = 0;
+        }
+    }, 5000);
+}
+
+function defaultSearch() {
+    var queryURL = "https://www.rijksmuseum.nl/api/en/collection/?key=nRpUKIg0&format=json&ps=10&imgonly=True&q=Van%20Gogh";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(data) {
+        console.log("Default firing!");
+        renderData(data);
+        $("#artQuery").val("");    
+    });
+}
 
 $(".submit").on("click", function(event) {
     event.preventDefault();
@@ -22,13 +59,13 @@ $(".submit").on("click", function(event) {
     login = username;
     var newPost = {
         name: username
-    }
+    };
     $.post("/", newPost, function(data) {
         $("#login").html("<div id='logWrap'>" + data.name + "&nbsp;&nbsp;<img id='avatar' src='/images/avatar.jpg'></div>");
         $("#username").val("");
+
         if(login) {
             $(".inputWrap").remove();
-            var slideshow;
             var newInput = $("<input id='artQuery' type='text'  placeholder='Type a masterpiece'>");
             var searchBtn = $("<button id='artSearch' class='btn btn-lg btn-danger' type='submit'>");
             searchBtn.text("Search");
@@ -52,37 +89,23 @@ $(".submit").on("click", function(event) {
                     $("#artQuery").val("");
                     return false;
                 } else {
-                    var queryURL = "https://www.rijksmuseum.nl/api/en/collection/?key=nRpUKIg0&format=json&ps=10&imgonly=True&q=" + 
-                    query;
-                    clearInterval(slideshow);
-                    $.ajax({
-                        url: queryURL,
-                        method: "GET"
-                    }).done(function(data) {
-                            console.log(data.artObjects);
-                            $("#artQuery").val("");
-                            var index = 1;
-                            var presentationDiv = $("<div>");
-                            var returnedName = $("<h1 class='artistName'>").text(data.artObjects[0].principalOrFirstMaker);
-                            var returnedTitle = $("<h1 class='artTitle'>").text(data.artObjects[0].title);
-                            var returnedImages = $("<img class='artDisplay'>").attr("src", data.artObjects[0].webImage.url);
-                            presentationDiv.append(returnedName, returnedTitle, returnedImages);
-                            $(".imageDisplay").html(presentationDiv);
-                                slideshow = setInterval(function() {
-                                var presentationDiv = $("<div>");
-                                var returnedName = $("<h1 class='artistName'>").text(data.artObjects[index].principalOrFirstMaker);
-                                var returnedTitle = $("<h1 class='artTitle'>").text(data.artObjects[index].title);
-                                var returnedImages = $("<img class='artDisplay'>").attr("src", data.artObjects[index].webImage.url);
-                                presentationDiv.append(returnedName, returnedTitle, returnedImages);
-                                $(".imageDisplay").html(presentationDiv);
-                                index += 1;
-                                if(index === data.artObjects.length) {
-                                    index = 0;
-                                }
-                            }, 5000);
-                        });
-                    }
-                });
+                var queryURL = "https://www.rijksmuseum.nl/api/en/collection/?key=nRpUKIg0&format=json&ps=10&imgonly=True&q=" + 
+                query;
+                console.log(queryURL);
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).done(function(data) {
+                        console.log(data.artObjects);
+                        if (data.artObjects.length === 0) {
+                            defaultSearch();
+                        } else {
+                            renderData(data);
+                        }
+                        $("#artQuery").val("");
+                    });
+                }
+            });
         }
     });
 });
